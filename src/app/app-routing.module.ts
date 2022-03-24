3d5794088515d938211c641621ad740cb3b6a304
vigneshwar <vigneshwar221b@gmail.com>
2022-03-24 09:03:29 +0530

    added routing and navbar v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import { GamingComponent } from './gaming/gaming.component';
+import { GroupsComponent } from './groups/groups.component';
+import { HomeComponent } from './home/home.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { ProfileComponent } from './profile/profile.component';
+import { WatchComponent } from './watch/watch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: AuthPageComponent },
+  { path: 'watch', component: WatchComponent },
+  { path: 'marketplace', component: MarketplaceComponent },
+  { path: 'groups', component: GroupsComponent },
+  { path: 'gaming', component: GamingComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { WatchComponent } from './watch/watch.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { GroupsComponent } from './groups/groups.component';
+import { GamingComponent } from './gaming/gaming.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    WatchComponent,
+    MarketplaceComponent,
+    GroupsComponent,
+    GamingComponent,
+    ProfileComponent,
+    AuthPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
+}
