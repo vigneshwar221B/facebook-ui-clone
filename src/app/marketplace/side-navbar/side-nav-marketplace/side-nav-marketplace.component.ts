b52d217ea87d44a7fdb363c993f85bb1e98fd3e9
keerthivasan <75238669+keerthivasanm20@users.noreply.github.com>
2022-03-28 17:53:30 +0530

    updated market categories

--- src/app/marketplace/side-navbar/side-nav-marketplace/side-nav-marketplace.component.ts
+++ src/app/marketplace/side-navbar/side-nav-marketplace/side-nav-marketplace.component.ts
@@ -118,127 +118,127 @@ export class SideNavMarketplaceComponent implements OnInit {
     background_size: '34px 1082px',//'26px 1400px',
     arrow_icon:false,
   }
-  // ,
-  // {
-  //   item: 'Clothing',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // }
-  // ,
-  // {
-  //   item: 'Electronics',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // },
-  // {
-  //   item: 'Enternaiment',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // },
-  // {
-  //   item: 'Family',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // },
-  // {
-  //   item: 'Free stuff',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // },
-  // {
-  //   item: 'Garden and Outdoors',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // },
-  // {
-  //   item: 'Hobbies',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // },
-  // {
-  //   item: 'Home goods',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // }
-  // ,
-  // {
-  //   item: 'Home improvement supplies',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // }
-  // ,
-  // {
-  //   item: 'Musical instruments',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // }
-  // ,
-  // {
-  //   item: 'Office supplies',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // }
-  // ,
-  // {
-  //   item: 'Pet supplies',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // }
-  // ,
-  // {
-  //   item: 'Property for sale',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // }
-  // ,
-  // {
-  //   item: 'Sporting goods',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // },
-  // {
-  //   item: 'Toys & games',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // }
-  // ,
-  // {
-  //   item: 'Buy-and-sell groups',
-  //   img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
-  //   background_position:'-22px -168px',//'0px -630px',
-  //   background_size: '64px 300px',//'26px 1400px',
-  //   arrow_icon:false,
-  // }
+  ,
+  {
+    item: 'Clothing',
+    img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'-22px -234px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  }
+  ,
+  {
+    item: 'Electronics',
+    img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/yy/r/-zxyuanMAV5.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'0px -668px',//'0px -630px',
+    background_size: '34px 1024px',//'26px 1400px',
+    arrow_icon:false,
+  },
+  {
+    item: 'Enternaiment',
+    img_url:'		https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'-22px -146px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  },
+  {
+    item: 'Family',
+    img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/yv/r/VNac1yDYOXZ.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'0px -598px',//'0px -630px',
+    background_size: ' 34px 1082px',//'26px 1400px',
+    arrow_icon:false,
+  },
+  {
+    item: 'Free stuff',
+    img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'0px -256px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  },
+  {
+    item: 'Garden and Outdoors',
+    img_url:'			https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'0px -278px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  },
+  {
+    item: 'Hobbies',
+    img_url:'		https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'-22px -124px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  },
+  {
+    item: 'Home goods',
+    img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/yy/r/-zxyuanMAV5.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'0px -580px',//'0px -630px',
+    background_size: '34px 1024px',//'26px 1400px',
+    arrow_icon:false,
+  }
+  ,
+  {
+    item: 'Home improvement supplies',
+    img_url:'		https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'-22px -278px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  }
+  ,
+  {
+    item: 'Musical instruments',
+    img_url:'		https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'-22px -168px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  }
+  ,
+  {
+    item: 'Office supplies',
+    img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'0px -168px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  }
+  ,
+  {
+    item: 'Pet supplies',
+    img_url:'		https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'0px -212px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  }
+  ,
+  {
+    item: 'Property for sale',
+    img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'-22px -168px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  }
+  ,
+  {
+    item: 'Sporting goods',
+    img_url:'		https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'-22px -212px',//'0px -630px',
+    background_size: '64px 300px',//'26px 1400px',
+    arrow_icon:false,
+  },
+  {
+    item: 'Toys & games',
+    img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/yy/r/-zxyuanMAV5.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'0px -558px',//'0px -630px',
+    background_size: '34px 1024px',//'26px 1400px',
+    arrow_icon:false,
+  }
+  ,
+  {
+    item: 'Buy-and-sell groups',
+    img_url:'	https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/HKwP8Wj1Tef.png',//https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/-wm-fqdtvDt.png
+    background_position:'0px -558px',//'0px -630px',
+    background_size: '34px 1024px',//'26px 1400px',
+    arrow_icon:false,
+  }
   
 ]
 
