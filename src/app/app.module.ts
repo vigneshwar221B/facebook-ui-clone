f997e658f0148da10680fa61d1d9324b19b53bf1
vigneshwar <vigneshwar221b@gmail.com>
2022-03-29 05:21:20 +0530

    added create post in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import { CreatePostComponent } from './home/create-post/create-post.component';
+import { CreatePostComponent } from './home/components/create-post/create-post.component';
 import { SideNavComponent } from './home/components/side-nav/side-nav.component';
 import { SideNavItemsComponent } from './home/components/side-nav-items/side-nav-items.component';
 import { SideNavShortcutsComponent } from './home/components/side-nav-shortcuts/side-nav-shortcuts.component';
