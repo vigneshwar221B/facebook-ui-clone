3a6c6835ea99af589c08abc8a15f52228b8701ae
vigneshwar <vigneshwar221b@gmail.com>
2022-03-29 15:21:51 +0530

    gaming: fixed the alignment bug

--- src/app/gaming/posts/posts.component.ts
+++ src/app/gaming/posts/posts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-gaming-posts',
@@ -7,28 +7,39 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./posts.component.scss'],
 })
 export class PostsComponent implements OnInit {
-  posts = [
-    {
-      name: 'name1',
-      game: 'game1',
-      date: 'Feb 1',
-      vidUrl: 'https://www.youtube.com/embed/p4QG59y6FGE',
-    },
-    {
-      name: 'name2',
-      game: 'game2',
-      date: 'Feb 2',
-      vidUrl: 'https://www.youtube.com/embed/-dCIPjxH3wU',
-    },
-    {
-      name: 'name3',
-      game: 'game3',
-      date: 'Feb 3',
-      vidUrl: 'https://www.youtube.com/embed/uIt298IgFhk',
-    },
-  ];
+  // posts: { name: string; game: string; date: string; vidUrl: string }[] = [
+  //   {
+  //     name: 'name1',
+  //     game: 'game1',
+  //     date: 'Feb 1',
+  //     vidUrl: 'https://www.youtube.com/embed/p4QG59y6FGE',
+  //   },
+  //   {
+  //     name: 'name2',
+  //     game: 'game2',
+  //     date: 'Feb 2',
+  //     vidUrl: 'https://www.youtube.com/embed/-dCIPjxH3wU',
+  //   },
+  //   {
+  //     name: 'name3',
+  //     game: 'game3',
+  //     date: 'Feb 3',
+  //     vidUrl: 'https://www.youtube.com/embed/uIt298IgFhk',
+  //   },
+  // ];
 
-  constructor(public sanitizer: DomSanitizer) {}
+  //safePosts: { name: string; game: string; date: string; vidUrl: SafeUrl }[];
+
+  constructor(public sanitizer: DomSanitizer) {
+    // this.safePosts = this.posts.map((post) => {
+    //   return {
+    //     name: post.name,
+    //     game: post.game,
+    //     date: post.date,
+    //     vidUrl: this.sanitizer.bypassSecurityTrustUrl(post.vidUrl),
+    //   };
+    // });
+  }
 
   ngOnInit(): void {}
 }
