3497ae20365923094366945bfab9200e9dbfd02d
keerthivasan <75238669+keerthivasanm20@users.noreply.github.com>
2022-03-29 11:59:00 +0530

    added watch service

---
+++ src/app/interfaces/Watch.ts
@@ -0,0 +1,14 @@
+
+
+export interface Watch {
+     
+    username:string;
+    user_dp:string;
+    no_of_like:string;
+    no_of_views:string;
+    no_of_comments:string;
+    posted_date:string;
+    video:string;
+
+ 
+ }
