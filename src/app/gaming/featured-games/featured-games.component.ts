226d674fcd8d66f0f68b4fdcb9ef745c7ea0a5b5
vigneshwar <vigneshwar221b@gmail.com>
2022-03-30 11:50:38 +0530

    gaming: added more games data

--- src/app/gaming/featured-games/featured-games.component.ts
+++ src/app/gaming/featured-games/featured-games.component.ts
@@ -8,54 +8,49 @@ import { Component, OnInit } from '@angular/core';
 export class FeaturedGamesComponent implements OnInit {
   games = [
     {
-      name: 'Quiz Planet',
-      img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
+      name: 'Minecraft',
+      img: 'https://dkofva0t6jnyn.cloudfront.net/sites/default/files/styles/amp_blog_image_large/public/consumer/blog/parentsultimateguide-minecraft-blog.jpeg',
+      genre: 'Adventure',
     },
     {
-      name: 'Quiz Planet',
-      img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
+      name: 'Valorant',
+      img: 'https://www.riotgames.com/darkroom/1000/9b3da9b650454646cf2a52a1635d5fe2:951146e9be67f6b31f556f888d7ac5a0/beta-key-art-valorant.jpg',
+      genre: 'FPS',
     },
     {
-      name: 'Quiz Planet',
-      img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
+      name: 'League of Legends',
+      img: 'https://assets-prd.ignimgs.com/2021/11/19/arcaneseasonone-blogroll-1637364360923.jpg',
+      genre: 'Online',
     },
     {
-      name: 'Quiz Planet',
-      img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
+      name: 'Elden ring',
+      img: 'https://image.api.playstation.com/vulcan/ap/rnd/202108/0410/2odx6gpsgR6qQ16YZ7YkEt2B.png',
+      genre: 'Online',
     },
     {
-      name: 'Quiz Planet',
+      name: 'Destiny',
       img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
+      genre: 'FPS',
     },
     {
-      name: 'Quiz Planet',
-      img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
+      name: 'GTA V',
+      img: 'https://upload.wikimedia.org/wikipedia/en/a/a5/Grand_Theft_Auto_V.png',
+      genre: 'Adventure',
     },
     {
-      name: 'Quiz Planet',
-      img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
+      name: 'Rea Dead Redemption',
+      img: 'https://media.rockstargames.com/rockstargames-newsite/uploads/d5c7e4dcecb612368aee64978f183250b6e643fe.jpg',
+      genre: 'Adventure',
     },
     {
-      name: 'Quiz Planet',
-      img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
+      name: 'Skyrim',
+      img: 'https://upload.wikimedia.org/wikipedia/en/1/15/The_Elder_Scrolls_V_Skyrim_cover.png',
+      genre: 'Action',
     },
     {
-      name: 'Quiz Planet',
-      img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
-    },
-    {
-      name: 'Quiz Planet',
-      img: 'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg',
-      genre: 'Quiz and Trivia',
+      name: 'Halo Infinite',
+      img: 'https://upload.wikimedia.org/wikipedia/en/1/14/Halo_Infinite.png',
+      genre: 'Shooting',
     }
   ];
   constructor() {}
